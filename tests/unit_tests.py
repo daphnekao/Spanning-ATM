9e76af24c81fa33a1a0a6432b005915095d40a1d
Daphne Kao <daphne.s.kao@gmail.com>
2017-07-11 12:06:48 -0500

    Feature/turn into package (#7)
    * Introduce infrastructure for making it runnable elsewhere
    
    * Add tests directory and preliminary README
    
    * Add unit testing shell

---
+++ tests/unit_tests.py
@@ -0,0 +1,59 @@
+import unittest
+
+class TestAccount(unittest.TestCase):
+    def test_execute_transactions(self):
+        pass
+
+    def test_log(self):
+        pass
+
+    def test_report_transaction_success(self):
+        pass
+
+    def test_deposit(self):
+        pass
+
+    def test_withdraw(self):
+        pass
+
+class TestCustomer(unittest.TestCase):
+    def test_display_account_choices(self):
+        pass
+
+    def test_update_account_summary(self):
+        pass
+
+    def test_display_account_summary(self):
+        pass
+
+class TestSession(unittest.TestCase):
+    def test_display_homescreen(self):
+        pass
+
+    def test_get_customer(self):
+        pass
+
+    def test_receipt(self):
+        pass
+
+    def test_login(self):
+        pass
+
+    def test_proceed(self):
+        pass
+
+    def test_serve(self):
+        pass
+
+    def test_logout(self):
+        pass
+
+class TestFormat(unittest.TestCase):
+    def test_clean(self):
+        pass
+
+    def test_dollar(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
