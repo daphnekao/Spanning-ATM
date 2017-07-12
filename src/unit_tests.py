f072d0cdb304cbb2cbdc90e1bd3855a62bcf3af0
Daphne Kao <daphne.s.kao@gmail.com>
2017-07-12 13:50:03 -0500

    Unit tests file adheres to PEP-8 now

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -9,6 +9,7 @@ from Account import Account
 from Customer import Customer
 from Session import Session
 
+
 class TestAccount(unittest.TestCase):
     def test_execute_transactions(self):
         pass
@@ -25,6 +26,7 @@ class TestAccount(unittest.TestCase):
     def test_withdraw(self):
         pass
 
+
 class TestCustomer(unittest.TestCase):
     def test_display_account_choices(self):
         pass
@@ -34,6 +36,7 @@ class TestCustomer(unittest.TestCase):
 
     def test_display_account_summary(self):
         pass
+
 
 class TestSession(unittest.TestCase):
     def test_display_homescreen(self):
@@ -57,12 +60,14 @@ class TestSession(unittest.TestCase):
     def test_logout(self):
         pass
 
+
 class TestUtils(unittest.TestCase):
     def test_clean(self):
         self.assertEqual(Utils.clean(" Black Power "), "black power")
 
     def test_dollar(self):
         self.assertEqual(Utils.dollar(54602.3), "$54,602.30")
+
 
 if __name__ == "__main__":
     unittest.main()
