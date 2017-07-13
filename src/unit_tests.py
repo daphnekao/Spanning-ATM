4f9768a3f74e1fa5effb382e479fb159d2bc98c1
Daphne Kao <daphne.s.kao@gmail.com>
2017-07-13 12:00:59 -0500

    Fill in unit tests and add TO-DOs

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -1,15 +1,25 @@
 """Unit tests for the application.
 
-**Note**: There are broken unit tests for cases that should be handled in
-the future. These are marked as expected failures for now. There are also
-a handful of methods that are very complicated and would require extensive
-mocking of the `raw_input()` function to test fully. They are marked for
-code review.
+TO-DOs:
+
+- There are broken unit tests for cases that should be handled in
+the future. These are marked as expected failures for now.
+
+- There are also methods that have the user interact with the program; I think
+that testing these fully would require some combination of mocking out the
+`raw_input()` function and exploiting the `doctest
+<https://docs.python.org/2/library/doctest.html>`_ library, which tests
+interactive Python examples. These tests are skipped for now and marked for
+code review feedback.
+
+- Finally, there are unit tests which are still in progress. These are skipped
+for now with the "In progress" message and will be updated in the next pull
+request.
 """
 
-import unittest
 import StringIO
 import sys
+import unittest
 from datetime import datetime
 
 import Utils
@@ -17,7 +27,7 @@ from Account import Account
 from Customer import Customer
 from Session import Session
 
-CODE_REVIEW_COMMENT = ("Function is too complex; should refactor and/or "
+REQUEST_CODE_REVIEW = ("Function is too complex; should refactor and/or "
     "discuss with others before proceeding.")
 
 class TestAccount(unittest.TestCase):
@@ -25,7 +35,27 @@ class TestAccount(unittest.TestCase):
     def setUp(self):
         self.account = Account("savings", 35.25)
 
-    def test_execute_transactions(self):
+    @unittest.skip(REQUEST_CODE_REVIEW)
+    def test_execute_transactions_answer_is_w(self):
+        # Assert that `account.withdraw()` was called.
+        pass
+
+    @unittest.skip(REQUEST_CODE_REVIEW)
+    def test_execute_transactions_answer_is_d(self):
+        # Assert that `account.deposit()` was called.
+        # Can re-use the `confirmPrintStatement()` function I wrote below.
+        pass
+
+    @unittest.skip(REQUEST_CODE_REVIEW)
+    def test_execute_transactions_answer_is_x(self):
+        # Assert that 'Canceling transaction.' was printed.
+        pass
+
+    @unittest.skip(REQUEST_CODE_REVIEW)
+    def test_execute_transactions_make_3_attempts(self):
+        # Move through the while loop 3 times and assert that
+        # 'Canceling transaction after 3 attempts' was printed.
+        # Again, can re-use `confirmPrintStatement()`.
         pass
 
     def test_log(self):
@@ -74,41 +104,105 @@ class TestAccount(unittest.TestCase):
         self.assertRaises(TypeError, account.report_transaction_success,
             "deposit", "string where there should be a float or an int")
 
-    @unittest.skip(CODE_REVIEW_COMMENT)
+    @unittest.skip(REQUEST_CODE_REVIEW)
     def test_deposit(self):
+        # Assert that the balance gets decremented.
+        # Assert that `account.report_transaction_success()` gets called with
+        # the right arguments.
+        # Assert that `log()` gets called with the right arguments.
         pass
 
-    @unittest.skip(CODE_REVIEW_COMMENT)
+    @unittest.expectedFailure
+    def test_deposit_negative_number(self):
+        pass
+
+    @unittest.expectedFailure
+    def test_deposit_not_a_number(self):
+        pass
+
+    @unittest.skip(REQUEST_CODE_REVIEW)
     def test_withdraw(self):
+        # Assert that balance was decremented.
+        # Assert that `account.report_transaction_success()` was called.
+        # Assert that `account.log()` was called.
         pass
 
+    @unittest.skip(REQUEST_CODE_REVIEW)
+    def test_withdraw_input_x(self):
+        # Assert that 'Transaction canceled.' was printed.
+        pass
+
+    @unittest.expectedFailure
+    def test_withdraw_over_200(self):
+        # Assert that 'You may not withdraw more than $200 in one session.'
+        # was printed.
+        # Assert that user is asked to try another amount.
+        pass
+
+    @unittest.expectedFailure
+    def test_withdraw_not_a_multiple_of_20(self):
+        # Assert that 'You must enter a multiple of $20.' was printed.
+        # Assert that user is asked to try another amount.
+        pass
+
+    @unittest.expectedFailure
+    def test_withdraw_negative_number(self):
+        pass
+
+    @unittest.expectedFailure
+    def test_withdraw_not_a_number(self):
+        pass
+
+    @unittest.skip(REQUEST_CODE_REVIEW)
+    def test_withdraw_overdraft(self):
+        # Assert that overdraft warning was printed.
+        # Assert that account.balance was decremented twice.
+        # Assert that `account.log()` was called twice.
+        pass
 
 class TestCustomer(unittest.TestCase):
     """Tests the `Customer.Customer` class."""
-    # def setUp():
-    #     accounts = [
-    #         {
-    #             "account_type": "checking",
-    #             "balance": 256.45
-    #         },
-    #         {
-    #             "account_type": "savings",
-    #             "balance": 100467.21
-    #         }
-    #     ]
-    #     self.customer = Customer("0121", "Worf", accounts)
+    def setUp(self):
+        accounts = [
+            {
+                "account_type": "checking",
+                "balance": 256.45
+            },
+            {
+                "account_type": "savings",
+                "balance": 100467.21
+            }
+        ]
+        self.customer = Customer("0121", "Worf", accounts)
 
     def test_display_account_choices(self):
-        pass
-
-    def test_display_account_choices_no_accounts(self):
-        pass
+        expected_output = (
+            "To access your checking account, enter c.\n"
+            "To access your savings account, enter s.\n"
+            )
+        actual_output = self.customer.display_account_choices()
+        self.assertEqual(actual_output, expected_output)
 
     def test_update_account_summary(self):
-        pass
+        expected_summary = [
+            "checking: $256.45 available",
+            "savings: $100,467.21 available"
+        ]
+        self.customer.update_account_summary()
+        self.assertEqual(self.customer.summary, expected_summary)
 
     def test_display_account_summary(self):
-        pass
+        self.customer.summary = [
+            "Pity the warrior who slays all his foes!",
+            "Revenge is a dish best served cold."
+        ]
+        expected_summary = (
+            "Here is your account summary: \n"
+            "Pity the warrior who slays all his foes!\n"
+            "Revenge is a dish best served cold.\n"
+            )
+        actual_summary = self.customer.display_account_summary()
+        self.assertEqual(actual_summary, expected_summary)
 
 
 class TestSession(unittest.TestCase):
@@ -170,14 +264,37 @@ class TestSession(unittest.TestCase):
         self.confirmPrintStatement(self.session.display_homescreen,
             "Welcome to UT Credit Union!")
 
-    def test_get_customer(self):
+    @unittest.skip(REQUEST_CODE_REVIEW)
+    def test_get_customer_with_short_pin_for_3_attemps(self):
+        # Assert that `raw_input` is called 3 additional times and
+        # that 'Canceling after 3 attempts' is printed.
         pass
 
-    def test_receipt(self):
-        # Confirm that update_account_summary() was called
+    @unittest.skip(REQUEST_CODE_REVIEW)
+    def test_get_customer_with_secret_code_xxxx(self):
+        # Assert that self.running equals False.
         pass
 
-    # Why is this failing?
+    @unittest.skip("In progress")
+    def test_receipt_1_transaction(self):
+        # Stub out `current_customer.update_account_summary()` and confirm it
+        # was called.
+        pass
+
+    @unittest.skip("In progress")
+    def test_receipt_1_transaction(self):
+        # Stub out `current_customer.update_account_summary()` and confirm it
+        # was called.
+        # Assert that 'You made 1 transaction today' was in the final output.
+        # Stub out current_customer.display_account_summary() and confirm it
+        # was called.
+        pass
+
+    @unittest.skip("In progress")
+    def test_receipt_multiple_transaction(self):
+        # Same as above but check for the plural version.
+        pass
+
     def test_greet_customer(self):
         expected_greeting = (
             "Hello, Worf!\n"
@@ -188,11 +305,11 @@ class TestSession(unittest.TestCase):
         self.confirmPrintStatement(self.session.greet_customer,
             expected_greeting)
 
+    @unittest.skip(REQUEST_CODE_REVIEW)
     def test_proceed(self):
         pass
 
-    @unittest.skip(CODE_REVIEW_COMMENT)
-    # Use doctest for this?
+    @unittest.skip(REQUEST_CODE_REVIEW)
     def test_serve(self):
         pass
 
