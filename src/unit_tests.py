71ac329381651d2fca59d258cc74e3aa982405e3
Daphne Kao <daphne.s.kao@gmail.com>
2017-07-12 23:08:09 -0500

    Added method for confirming print statements

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -1,10 +1,12 @@
 """Unit tests for the application.
 
 **Note**: There are broken unit tests for cases that should be handled in
-the future.
+the future. These are marked as expected failures for now.
 """
 
 import unittest
+import StringIO
+import sys
 from datetime import datetime
 
 import Utils
@@ -15,62 +17,62 @@ from Session import Session
 
 class TestAccount(unittest.TestCase):
     """Tests the `Account.Account` class."""
+    def setUp(self):
+        self.account = Account("savings", 35.25)
+
     def test_execute_transactions(self):
         pass
 
     def test_log(self):
         """TO-DO: Write a time-freezing decorator or use a 3rd-party
-        time-freezing library like Python's `freezegun` to capture the
-        full time stamp. Then we can directly compare a full expected
-        log entry to the actual entry instead of validating the individual
-        `transaction_type` and `amount` values, then confirming that the time
-        stamp matches the current time rounded to the nearest minute.
+        time-freezing library like Python's `freezegun` to capture the full
+        time stamp. Then we can directly compare full log entries instead of
+        validating the individual `transaction_type` and `amount` values,
+        then confirming that the time stamps match to the nearest minute.
         """
-        account = Account("savings", 35.25)
-        account.log("deposit", 5.25)
-        self.assertEqual(len(account.history), 1)
-        only_entry = account.history[0]
+        self.account.log("deposit", 5.25)
+        self.assertEqual(len(self.account.history), 1)
+        only_entry = self.account.history[0]
         current_time = str(datetime.now())
         self.assertEqual(only_entry["transaction_type"], "deposit")
         self.assertEqual(only_entry["amount"], "$5.25")
         self.assertEqual(only_entry["time"][0:14], current_time[0:14])
 
     def test_log_multiple_transactions(self):
-        account = Account("money market", 57.89)
-        account.log("deposit", 3.41)
-        account.log("withdrawal", 2.00)
-        self.assertEqual(len(account.history), 2)
-        self.assertEqual(account.history[0]["transaction_type"], "deposit")
-        self.assertEqual(account.history[1]["transaction_type"], "withdrawal")
+        self.account.log("deposit", 3.41)
+        self.account.log("withdrawal", 2.00)
+        self.assertEqual(len(self.account.history), 2)
+        self.assertEqual(self.account.history[0]["transaction_type"],
+            "deposit")
+        self.assertEqual(self.account.history[1]["transaction_type"],
+            "withdrawal")
 
-    # Note that for these next three tests, the balance will not chance until
-    # self.balance is incremented or decremented.
+    # For these next 3 tests, the balance will not change until self.balance
+    # is incremented or decremented. Perhaps something to think about.
     def test_report_transaction_success_withdrawal(self):
-        account = Account("money market", 57.89)
-        actual_report = account.report_transaction_success("withdrawal", 7.89)
-        expected_report = "Successfully withdrew $7.89. New balance: $57.89"
+        actual_report = self.account.report_transaction_success("withdrawal",
+            7.89)
+        expected_report = "Successfully withdrew $7.89. New balance: $35.25"
         self.assertEqual(actual_report, expected_report)
 
     def test_report_transaction_success_deposit(self):
-        account = Account("money market", 57.89)
-        actual_report = account.report_transaction_success("deposit", 0.11)
-        expected_report = "Successfully deposited $0.11. New balance: $57.89"
+        actual_report = self.account.report_transaction_success("deposit", 0.11)
+        expected_report = "Successfully deposited $0.11. New balance: $35.25"
         self.assertEqual(actual_report, expected_report)
 
     def test_report_transaction_success_invalid_transaction(self):
-        account = Account("money market", 57.89)
-        self.assertRaises(ValueError, account.report_transaction_success,
+        self.assertRaises(ValueError, self.account.report_transaction_success,
             "nonsense", 3.06)
 
+    @unittest.expectedFailure
+    def test_report_transaction_success_invalid_amount(self):
+        self.assertRaises(TypeError, account.report_transaction_success,
+            "deposit", "string where there should be a float or an int")
+
     def test_deposit(self):
-        # account = Account("checking", 100.00)
-        # account.deposit()
-        # self.assertEqual()
-        # Use mocks
         pass
 
     def test_withdraw(self):
-        # Use mocks
         pass
 
 
@@ -88,8 +90,58 @@ class TestCustomer(unittest.TestCase):
 
 class TestSession(unittest.TestCase):
     """Tests the `Session.Session` class."""
+    customer_list = [
+        {
+            "name": "Jean-Luc Picard",
+            "pin": "4145",
+            "accounts": [
+                {
+                    "account_type": "checking",
+                    "balance": 53.98
+                },
+                {
+                    "account_type": "money market",
+                    "balance": 2000000.00
+                },
+                {
+                    "account_type": "savings",
+                    "balance": 650782.24
+                }
+            ]
+        },
+        {
+            "name": "Worf",
+            "pin": "0121",
+            "accounts": [
+                {
+                    "account_type": "checking",
+                    "balance": 256.45
+                },
+                {
+                    "account_type": "savings",
+                    "balance": 100467.21
+                }
+            ]
+        }
+    ]
+    def setUp(self):
+        self.session = Session(self.customer_list, "UT Credit Union")
+        self.session.current_customer = Customer("0121", "Worf",
+            self.customer_list[1]["accounts"])
+
+    def confirmPrintStatement(self, func, expected_output):
+        captured_output = StringIO.StringIO()
+        sys.stdout = captured_output
+        func()
+        sys.stdout = sys.__stdout__
+        # Because Python's `print` automatically appends a newline...
+        expected_output += "\n"
+        self.assertEqual(captured_output.getvalue(), expected_output)
+        captured_output.close()
+
     def test_display_homescreen(self):
-        pass
+        self.confirmPrintStatement(self.session.display_homescreen,
+            "Welcome to UT Credit Union!")
 
     def test_get_customer(self):
         pass
@@ -97,8 +149,17 @@ class TestSession(unittest.TestCase):
     def test_receipt(self):
         pass
 
-    def test_login(self):
-        pass
+    # Why is this failing?
+    def test_greet_customer(self):
+        expected_greeting = (
+            "Hello, Worf!\n"
+            "Here is your account summary: \n"
+            "'checking': $256.45 available\n"
+            "'savings': $100,467.21 available\n"
+            "\n"
+            )
+        self.confirmPrintStatement(self.session.greet_customer,
+            expected_greeting)
 
     def test_proceed(self):
         pass
@@ -107,7 +168,8 @@ class TestSession(unittest.TestCase):
         pass
 
     def test_logout(self):
-        pass
+        self.confirmPrintStatement(self.session.logout,
+            "Thank you for banking with UT Credit Union. Good bye!")
 
 
 class TestUtils(unittest.TestCase):
